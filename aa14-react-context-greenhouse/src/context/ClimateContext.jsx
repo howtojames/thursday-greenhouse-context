f4f902cf5b1f341bff5c15949ea67645fe190c77
Donald Roessler <me@donaldroessler.com>
2023-11-09 13:21:02 -0800

    phase 4 done

--- aa14-react-context-greenhouse/src/context/ClimateContext.jsx
+++ aa14-react-context-greenhouse/src/context/ClimateContext.jsx
@@ -1,2 +1,17 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { createContext, useState } from "react";
+
+
+export const ClimateContext = createContext();
+
+export default function ClimateProvider( { children }) {
+  const [temperature, setTemperature] = useState(50);
+
+  return (
+    <ClimateContext.Provider value={{ temperature, setTemperature }}>
+      {children}
+    </ClimateContext.Provider>
+  )
+}
--- aa14-react-context-greenhouse/src/main.jsx
+++ aa14-react-context-greenhouse/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import ThemeProvider from './context/ThemeContext';
+import ClimateProvider from './context/ClimateContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <ThemeProvider>
-        <App />
+        <ClimateProvider>
+          <App />
+        </ClimateProvider>
       </ThemeProvider>
   </React.StrictMode>
 );
